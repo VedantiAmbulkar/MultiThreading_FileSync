1b6515dd04cc15250603c0e115397e38423fce14
WorkWithSoham <sohamtembe98@gmail.com>
2023-07-03 16:22:38 -0500

    Working client and server model posted.

---
+++ src/main/java/Constants.java
@@ -0,0 +1,11 @@
+import java.nio.file.Path;
+
+public abstract class Constants {
+    public static final String HOST = "http://localhost";
+    public static final int ServerPort = 8000;
+    public static final int ClientAPort = 8001;
+    public static final int ClientBPort = 8002;
+
+    public static final String serverFileHolder = "serverFileHolder/";
+    public static final String clientFileHolder = "clientFileHolder/";
+}
